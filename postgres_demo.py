8a824f33bc8b4784993295664a59fa5106952f6e
elenajohannessen <elenajohannessen@yahoo.com>
2023-07-12 21:42:28 -0400

    Complete project
    Signed-off-by: elenajohannessen <elenajohannessen@yahoo.com>

--- postgres_demo.py
+++ postgres_demo.py
@@ -18,3 +18,29 @@ con = psycopg2.connect(
     password="postgres")
 
 print(con)
+
+#For isolation: SERIALIZABLE
+con.set_isolation_level(3)
+
+#For atomicity
+con.autocommit = False
+
+try:
+    cur = con.cursor()
+    # QUERY
+    cur.execute("insert into depot(dep,addr, volume) select 'dd1', addr, volume from depot where dep = 'd1'")
+    cur.execute("update stock set dep = 'dd1' where dep = 'd1'")
+    cur.execute("delete from depot where dep = 'd1'")
+
+except (Exception, psycopg2.DatabaseError) as err:
+    print(err)
+    print("Transactions could not be completed so database will be rolled back before start of transactions")
+    con.rollback()
+finally:
+    if con:
+        con.commit()
+        cur.close
+        con.close
+        print("PostgreSQL connection is now closed")
+
+print("End")
